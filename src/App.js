7e7563c8e0a246e51029aa28e9abaeda2be92b18
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-05-27 04:28:24 -0300

    Add config for webpack alias.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import Login from './pages/Login';
+import MainPage from 'pages/MainPage';
+import Login from 'pages/Login';
 
 
 const App = () => (
