5df58ac50b5c6001a998829651fd6b33c929645b
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-05-27 04:40:16 -0300

    Add initial styles.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
+
 import MainPage from 'pages/MainPage';
 import Login from 'pages/Login';
 
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/" component={MainPage} />
-    </Switch>
-  </BrowserRouter>
+  <Fragment>
+    <CssBaseline />
+
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" component={MainPage} />
+      </Switch>
+    </BrowserRouter>
+  </Fragment>
 );
 
 export default App;
