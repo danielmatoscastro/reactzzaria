4a8dfbe6797ed32db2d33655d36948131df060d7
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-05-28 23:00:34 -0300

    Isolating firebase config.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,21 +2,8 @@ import React, {
   Fragment, useState, useEffect, useCallback,
 } from 'react';
 import { Grid, Button } from '@material-ui/core';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'services/firebase';
 import { Container, MainLogo, GitHubButton } from './style';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBzGc7us8I0XjmxT-j0JOYtm-f5qtIXM3U',
-  authDomain: 'reactzzaria-25738.firebaseapp.com',
-  databaseURL: 'https://reactzzaria-25738.firebaseio.com',
-  projectId: 'reactzzaria-25738',
-  storageBucket: 'reactzzaria-25738.appspot.com',
-  messagingSenderId: '398846638007',
-  appId: '1:398846638007:web:e0acec29ab78cbcb471a32',
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 function Login() {
   const [userInfo, setUserInfo] = useState({
---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBzGc7us8I0XjmxT-j0JOYtm-f5qtIXM3U',
+  authDomain: 'reactzzaria-25738.firebaseapp.com',
+  databaseURL: 'https://reactzzaria-25738.firebaseio.com',
+  projectId: 'reactzzaria-25738',
+  storageBucket: 'reactzzaria-25738.appspot.com',
+  messagingSenderId: '398846638007',
+  appId: '1:398846638007:web:e0acec29ab78cbcb471a32',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
