dcea662240054bd6e3a27750c4fa4582db1b7996
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-06-05 02:10:40 -0300

    Changing to get config data from .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBzGc7us8I0XjmxT-j0JOYtm-f5qtIXM3U',
-  authDomain: 'reactzzaria-25738.firebaseapp.com',
-  databaseURL: 'https://reactzzaria-25738.firebaseio.com',
-  projectId: 'reactzzaria-25738',
-  storageBucket: 'reactzzaria-25738.appspot.com',
-  messagingSenderId: '398846638007',
-  appId: '1:398846638007:web:e0acec29ab78cbcb471a32',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
